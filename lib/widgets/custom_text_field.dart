8586ce1322332676089b14ca4ddc5fbf6e1d2b07
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-03-14 01:52:10 +0200

    create custom text field

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:note_app/constants.dart';
 
-class CustomFormTextField extends StatelessWidget {
-  const CustomFormTextField({
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
     Key? key,
     required this.labelText,
   }) : super(key: key);
@@ -11,24 +13,31 @@ class CustomFormTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: EdgeInsets.symmetric(horizontal: 8.h, vertical: 24.w),
       child: TextField(
+        cursorColor: kPrimaryColor,
         decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(
-              width: 2,
-              color: Colors.white,
-            ),
-            borderRadius: BorderRadius.circular(5),
-          ),
-          contentPadding: const EdgeInsets.all(20),
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(kPrimaryColor),
+          contentPadding: const EdgeInsets.all(16),
           labelText: labelText,
-          labelStyle: const TextStyle(
-            color: Colors.white,
+          labelStyle: TextStyle(
+            color: kPrimaryColor,
             fontWeight: FontWeight.w500,
           ),
         ),
       ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2,
+        color: color ?? Colors.white,
+      ),
+      borderRadius: BorderRadius.circular(8),
     );
   }
 }
--- lib/widgets/model_bottom_sheet.dart
+++ lib/widgets/model_bottom_sheet.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
 
 class AddModelBottomSheet extends StatelessWidget {
   const AddModelBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: const Column(
+          children: [
+            CustomTextField(labelText: 'Title'),
+          ],
+        ),
       ),
     );
   }
